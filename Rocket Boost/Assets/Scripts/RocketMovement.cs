f4e754334a6e25c004f3f14ec65de22f0024c336
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-10-13 17:12:41 -0400

    Basic Rocket Thrust

--- Rocket Boost/Assets/Scripts/RocketMovement.cs
+++ Rocket Boost/Assets/Scripts/RocketMovement.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 
 public class RocketMovement : MonoBehaviour
 {
+    Rigidbody myRigidbody;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        RotateRocket();
+        ThrustRocket();
+    }
+
+    void RotateRocket()
+    {
+        if (Input.GetKey(KeyCode.A))
+            Debug.Log("Left");
+
+        else if (Input.GetKey(KeyCode.D))
+            Debug.Log("Right");
+    }
+
+    void ThrustRocket()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            myRigidbody.AddRelativeForce(Vector3.up);
     }
 }
