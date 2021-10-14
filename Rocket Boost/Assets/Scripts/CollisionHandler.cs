05b28725b9226f307d520951004f021605c2c03d
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-10-14 15:17:39 -0400

    Level Management

--- Rocket Boost/Assets/Scripts/CollisionHandler.cs
+++ Rocket Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,26 +1,83 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float nextLevelDelay = 2f;
+    [SerializeField] float crashDelay = 2f;
+
+    RocketMovement rocketMovementScript;
+
+    void Start()
+    {
+        rocketMovementScript = GetComponent<RocketMovement>();
+    }
+
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("This is friendly.");
+                // TODO: Small Glow VFX?
                 break;
             
             case "Fuel":
-                Debug.Log("Refueling...");
+                // TODO: Add Fuel
                 break;
             
             case "Finish":
-                Debug.Log("Finish!");
+                StartCoroutine(StartVictorySequence(nextLevelDelay));
                 break;
             
             default:
-                Debug.Log("Dead");
+                StartCoroutine(StartCrashSequence(crashDelay));
                 break;
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        int totalNumberOfScenes = SceneManager.sceneCountInBuildSettings;
+
+        if (nextSceneIndex == totalNumberOfScenes)
+            SceneManager.LoadScene(0);
+        else
+            SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    IEnumerator StartVictorySequence(float waitTime)
+    {
+        // TODO: Add Victory SFX
+        // TODO: Add Victory VFX
+        rocketMovementScript.GetComponent<AudioSource>().Stop();
+        rocketMovementScript.enabled = false;
+
+        yield return new WaitForSeconds(waitTime);
+
+        rocketMovementScript.enabled = true;
+        LoadNextLevel();
+    }
+
+    IEnumerator StartCrashSequence(float waitTime)
+    {
+        // TODO: Add Crash SFX
+        // TODO: Add Crash VFX
+        rocketMovementScript.GetComponent<AudioSource>().Stop();
+        rocketMovementScript.enabled = false;
+
+        yield return new WaitForSeconds(waitTime);
+
+        rocketMovementScript.enabled = true;
+        ReloadLevel();
     }
 }
