f6b63abe54fdc760533ae226f78ce84eef1c39e9
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-10-18 13:38:06 -0400

    Temporary Quit Mechanic

---
+++ Rocket Boost/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+    }
+}
